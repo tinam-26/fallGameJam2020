b231bb2743438c05da9c2b86e2dd183c52297ae6
tpneumann <tneumann@uncc.edu>
2020-10-31 17:38:00 -0400

    HintRoom1 spawning works!

--- Assets/Scripts/HintRoomSpawners/HintRoom1.cs
+++ Assets/Scripts/HintRoomSpawners/HintRoom1.cs
@@ -12,7 +12,7 @@ public class HintRoom1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //correctDir selects the correct direction to move in, 0 for right, 1 for up, 2 for left and 3 for down
+        //correctDir selects the correct direction to move in, 0 for left, 1 for up, 2 for right and 3 for down
         correctDir = Random.Range(0, 4);
 
         hints[correctDir].SetActive(false);
@@ -28,15 +28,109 @@ public class HintRoom1 : MonoBehaviour
         }
 
         //TODO figure out which door was spawned at the top/left/ectmost location and switch locations with the correct door
+        //0 for left, 1 for up, 2 for right and 3 for down
+        int next = 0;
         switch (correctDir)
         {
             case 0:
+                float smallestX = doors[0].transform.position.x;
+
+                for (int i = 1; i < doors.Length; i++)
+                {
+                    if (doors[i].transform.position.x < smallestX)
+                    {
+                        smallestX = doors[i].transform.position.x;
+                        next = i;
+                    }
+                    //if two doors are equally leftmost, move one over to the right
+                    else if (doors[i].transform.position.x == smallestX)
+                    {
+                        Vector2 nextPos = new Vector2(doors[i].transform.position.x + .5f, doors[i].transform.position.y);
+                        doors[i].transform.position = nextPos;
+                    }
+                }
+
+                if (next != 0)
+                {
+                    Vector2 origPos = new Vector2(doors[0].transform.position.x, doors[0].transform.position.y);
+                    doors[0].transform.position = doors[next].transform.position;
+                    doors[next].transform.position = origPos;
+                }
                 break;
+
             case 1:
+                float biggestY = doors[0].transform.position.y;
+
+                for (int i = 1; i < doors.Length; i++)
+                {
+                    if (doors[i].transform.position.y > biggestY)
+                    {
+                        biggestY = doors[i].transform.position.y;
+                        next = i;
+                    }
+                    else if (doors[i].transform.position.y == biggestY)
+                    {
+                        Vector2 nextPos = new Vector2(doors[i].transform.position.x, doors[i].transform.position.y - .5f);
+                        doors[i].transform.position = nextPos;
+                    }
+                }
+
+                if (next != 0)
+                {
+                    Vector2 origPos = new Vector2(doors[0].transform.position.x, doors[0].transform.position.y);
+                    doors[0].transform.position = doors[next].transform.position;
+                    doors[next].transform.position = origPos;
+                }
                 break;
+
             case 2:
+                float biggestX = doors[0].transform.position.x;
+
+                for (int i = 1; i < doors.Length; i++)
+                {
+                    if (doors[i].transform.position.x > biggestX)
+                    {
+                        smallestX = doors[i].transform.position.x;
+                        next = i;
+                    }
+                    else if (doors[i].transform.position.x == biggestX)
+                    {
+                        Vector2 nextPos = new Vector2(doors[i].transform.position.x - .5f, doors[i].transform.position.y);
+                        doors[i].transform.position = nextPos;
+                    }
+                }
+
+                if (next != 0)
+                {
+                    Vector2 origPos = new Vector2(doors[0].transform.position.x, doors[0].transform.position.y);
+                    doors[0].transform.position = doors[next].transform.position;
+                    doors[next].transform.position = origPos;
+                }
                 break;
+
             case 3:
+                float smallestY = doors[0].transform.position.x;
+
+                for (int i = 1; i < doors.Length; i++)
+                {
+                    if (doors[i].transform.position.x < smallestY)
+                    {
+                        smallestY = doors[i].transform.position.y;
+                        next = i;
+                    }
+                    else if (doors[i].transform.position.y == smallestY)
+                    {
+                        Vector2 nextPos = new Vector2(doors[i].transform.position.x, doors[i].transform.position.y + .5f);
+                        doors[i].transform.position = nextPos;
+                    }
+                }
+
+                if (next != 0)
+                {
+                    Vector2 origPos = new Vector2(doors[0].transform.position.x, doors[0].transform.position.y);
+                    doors[0].transform.position = doors[next].transform.position;
+                    doors[next].transform.position = origPos;
+                }
                 break;
         }
     }
